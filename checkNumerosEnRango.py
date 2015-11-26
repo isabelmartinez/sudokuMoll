9cdb26e1e1afcf1d8ae0ca02308b5dcb55c9f070
Dani Carril <Dani Carril>
2015-11-26 18:41:14 +0100

    Añadida la funcionalidad de comprobar si los datos introducidos son numeros enteros y estan dentro del rango de numero máximo de filas

--- checkNumerosEnRango.py
+++ checkNumerosEnRango.py
@@ -2,6 +2,16 @@
 def checkNumerosEnRango(sudoku):
 
     # precondiciones
+    for fila in sudoku:
+        for elemento in fila:
+            if isinstance(elemento, int):
+                if elemento < 1 or elemento > len(fila):
+                    return False
+                else:
+                    pass
+            else:
+                return False
+    return True
 
     # postcondiciones
 
@@ -10,4 +20,37 @@ def checkNumerosEnRango(sudoku):
 
 if __name__ == '__main__':
 
-  import casosTestSudoku
+    from casosTestSudoku import *
+
+    if checkNumerosEnRango(correct):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(incorrect):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(incorrect2):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(incorrect3):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(incorrect4):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(incorrect5):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(irregular):
+        print("OK")
+    else:
+        print("FAIL")
+    if checkNumerosEnRango(irregular2):
+        print("OK")
+    else:
+        print("FAIL")
